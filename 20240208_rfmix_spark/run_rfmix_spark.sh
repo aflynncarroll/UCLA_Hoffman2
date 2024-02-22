cc2176b8b8dbe202371ae6964d7b9ee858ff53c1
Alexander Flynn-Carroll <afcarrol@login3.hoffman2.idre.ucla.edu>
2024-02-22 10:26:55 -0800

    rfmix updates SPARK

--- 20240208_rfmix_spark/run_rfmix_spark.sh
+++ 20240208_rfmix_spark/run_rfmix_spark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
+#$ -N rfmix_spark
 #$ -cwd
-#$ -l h_data=120G,h_rt=16:00:00,highp
+#$ -l h_data=500,h_rt=23:00:00,highp
 #$ -j y
 #$ -o ./job_out
 #$ -t 1-22
@@ -11,29 +12,28 @@ module load anaconda3/2020.11
 conda activate plink2_env
 module load bcftools
 module load htslib
-
+#chrom=1
 chrom=${SGE_TASK_ID}
 REF_DIR=/u/project/pasaniuc/pasaniucdata/DATA/1000_Genomes_30x_GRCh38_phased
 RFMIX=/u/project/pasaniuc/kangchen/software/rfmix/rfmix
-pfile=/u/home/a/afcarrol/project-pasaniuc/Projects/16022022_ancestry/pgen_files/chr${chrom}
-out_prefix=/u/home/a/afcarrol/project-pasaniuc/Projects/16022022_ancestry/rfmix-lanc/chr${chrom}
-# /u/home/a/afcarrol/project-pasaniuc/Projects/20220216_ancestry/rfmix-lanc/
-# changed after the fact
+pfile=/u/project/geschwind/shared/GenomicDatasets-processed/ACE-ANALYSIS/freeze0/SPARK/imputed/chr${chrom}
+#out_prefix=/u/home/a/afcarrol/project-pasaniuc/Projects/20240208_rfmix_spark/out/chr${chrom}
+out_prefix=/u/project/geschwind/shared/PRS/20240221_SPARK_RFMix/chr${chrom}
 
 #mkdir -p /u/home/a/afcarrol/project-pasaniuc/Projects/16022022_ancestry/rfmix-lanc
 
 plink2 --pfile ${pfile} \
     --chr ${chrom} \
     --output-chr 26 \
-    --export vcf bgz \
+    --export vcf bgz id-delim=_ \
     --out ${out_prefix}.tmp
 
 tabix -p vcf ${out_prefix}.tmp.vcf.gz
 
 # filter EUR and AFR samples
 # extract 1st and 2nd column if 2nd column is equal to CEU or YRI, separated by tab
-#awk '$2=="CEU" || $2=="YRI" {print $1 "\t" $2}' ${REF_DIR}/out/metadata/sample_map.unrelated.tsv >${out_prefix}.tmp.sample_map.tsv
-awk '$2=="CEU" || $2=="YRI" || $2=="PEL" {print $1 "\t" $2}' ${REF_DIR}/out/metadata/sample_map.unrelated.tsv >${out_prefix}.tmp.sample_map.tsv
+awk '$2=="CEU" || $2=="YRI" {print $1 "\t" $2}' ${REF_DIR}/out/metadata/sample_map.unrelated.tsv >${out_prefix}.tmp.sample_map.tsv
+
 # run RFmix
 ${RFMIX} \
     -f ${out_prefix}.tmp.vcf.gz \
